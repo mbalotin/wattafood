6b132f67f817ad0815415a201d84bfcf22fc452a
Mateus Vendramini <mateusvendramini21@gmail.com>
2018-07-03 19:42:40 -0300

    release candidate 1

--- impl/gui/src/guipkg/AcompanharDietasPanel.java
+++ impl/gui/src/guipkg/AcompanharDietasPanel.java
@@ -5,6 +5,10 @@
  */
 package guipkg;
 
+import database.Consulta;
+import java.util.ArrayList;
+import javax.swing.DefaultListModel;
+
 /**
  *
  * @author mateus
@@ -30,23 +34,100 @@ public class AcompanharDietasPanel extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel5 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jList1 = new javax.swing.JList<>();
+        jButton5 = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel5.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
+        jLabel5.setText("Acompanhamento");
+
+        jList1.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
+        jScrollPane1.setViewportView(jList1);
+
+        jButton5.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
+        jButton5.setText("Analisar");
+        jButton5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton5ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(29, 29, 29)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 475, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(165, 165, 165)
+                        .addComponent(jLabel5))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(217, 217, 217)
+                        .addComponent(jButton5)))
+                .addContainerGap(27, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(jLabel5)
+                .addGap(31, 31, 31)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(29, 29, 29)
+                .addComponent(jButton5)
+                .addContainerGap(19, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
+
+        ArrayList<ArrayList<String>> refeicoes_feitas = new ArrayList<ArrayList<String>>();
+        ArrayList<ArrayList<ArrayList<String>>> refeicoes_dietas = new ArrayList<ArrayList<ArrayList<String>>>();
+
+        refeicoes_feitas = sessao.dbintf.getRegistroRefeicoes();
+        refeicoes_dietas = sessao.dbintf.getRefeicoesTodasDietas();
+
+        DefaultListModel model = new DefaultListModel();
+
+        // refeicoes feitas
+        for (ArrayList<String> rf: refeicoes_feitas){
+
+            // match refeicoes feitas contra dietas
+            for (ArrayList<ArrayList<String>> d: refeicoes_dietas){
+
+                // refeicoes em d (dieta)
+                for (ArrayList<String> r: d){
+                    boolean match = Consulta.alimentos_match(r, rf);
+                    String line = "Refeição: " + rf.toString() + " : ";
+                    if (match) {
+                        line += "OK";
+                    } else {
+                        line += "FAIL";
+                    }
+                    model.addElement(line);
+                }
+
+            }
+
+        }
+
+        jList1.setModel(model);
+
+    }//GEN-LAST:event_jButton5ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton5;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JList<String> jList1;
+    private javax.swing.JScrollPane jScrollPane1;
     // End of variables declaration//GEN-END:variables
 }
